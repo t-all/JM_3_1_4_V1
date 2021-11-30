0b704e1abecbf2f74ffe31ba17b6b64b2b90e72f
t-all <t-all@yandex.ru>
2021-11-30 11:11:58 +0300

    2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,42 +1,16 @@
 package org.example;
 
 import org.example.config.Config;
-import org.example.model.User;
 import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.http.ResponseEntity;
 
-import java.util.List;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
 
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigReactiveWebApplicationContext(Config.class);
         Communication communication = context.getBean("communication", Communication.class);
 
-//        List<User> allUsers = communication.getAllUsers();
-//        System.out.println(allUsers);
-
-//        User user = new User(3L, "James", "Brown", (byte) 34);
-//        ResponseEntity<String> saveUser = communication.saveUser(user);
-//        System.out.println(saveUser.getBody());
-
-//        User user1 = new User( 3L, "Thomas", "Shelby", (byte) 34);
-////        user1.setId(3L);
-//        ResponseEntity<String> editUser = communication.editUser(user1);
-//        ResponseEntity<String> up = communication.updateUser();
-//        System.out.println(up.getBody());
-//
-//        ResponseEntity<String> deleteUser = communication.deleteUser(3L);
-//        System.out.println(deleteUser.getBody());
-        System.out.println(communication.all());
-
-
+        System.out.println(communication.getAnswer());
     }
 }
--- src/main/java/org/example/Communication.java
+++ src/main/java/org/example/Communication.java
@@ -22,32 +22,7 @@ public class Communication {
         this.headers.set("Cookie", String.join(";", restTemplate.headForHeaders(URL).get("Set-Cookie")));
     }
 
-//    public List<User> getAllUsers() {
-//        ResponseEntity<List<User>> responseEntity =
-//                restTemplate.exchange(URL, HttpMethod.GET, null,
-//                        new ParameterizedTypeReference<List<User>>() {
-//                        });
-//
-//        List<User> allUser = responseEntity.getBody();
-//        //cookie
-//        RestTemplate template = new RestTemplate();
-//        ResponseEntity<String> forEntity = template.getForEntity(URL, String.class);
-//        forEntity.getHeaders().get("Set-Cookie").stream().forEach(System.out::println);
-//        headers.set("Cookie", String.join(";", restTemplate.headForHeaders(URL).get("Set-Cookie")));
-//        return allUser;
-//    }
-
-//    public User getUser(Long id) {
-//      User user = restTemplate.getForObject(URL + "/" + id, User.class);
-//
-//        headers.set("Cookie", String.join(";", restTemplate.headForHeaders(URL).get("Set-Cookie")));
-//
-//        return user;
-//    }
-
     public String saveUser() {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.set("Cookie", String.join(";", restTemplate.headForHeaders(URL).get("Set-Cookie")));
         System.out.println(headers);
         User user = new User(3L, "James", "Brown", (byte) 34);
         HttpEntity<User> entity = new HttpEntity<>(user, headers);
@@ -55,39 +30,21 @@ public class Communication {
         return restTemplate.exchange(URL, HttpMethod.POST, entity, String.class).getBody();
     }
 
-//    public ResponseEntity<String> editUser(User user) {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.set("Cookie", String.join(";", restTemplate.headForHeaders(URL).get("Set-Cookie")));
-//
-//        HttpEntity<User> entity = new HttpEntity<>(user, headers);
-//
-//        return restTemplate.exchange(URL+ "/" + user.getId(), HttpMethod.PUT, entity, String.class);
-//    }
-
     public String updateUser() {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.set("Cookie", String.join(";", restTemplate.headForHeaders(URL).get("Set-Cookie")));
         System.out.println(headers);
-        User user = new User(3L, "Thomas", "Shelby", (byte)34);
+        User user = new User(3L, "Thomas", "Shelby", (byte) 34);
         HttpEntity<User> entity = new HttpEntity<>(user, headers);
         return restTemplate.exchange(URL, HttpMethod.PUT, entity, String.class, 3L).getBody();
     }
 
     public String deleteUser() {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.set("Cookie", String.join(";", restTemplate.headForHeaders(URL).get("Set-Cookie")));
         System.out.println(headers);
         HttpEntity<User> entity = new HttpEntity<>(headers);
-
-        return restTemplate.exchange(URL+ "/"+3L, HttpMethod.DELETE, entity, String.class).getBody();
-
-
-
+        return restTemplate.exchange(URL + "/" + 3L, HttpMethod.DELETE, entity, String.class).getBody();
     }
 
-    public String all() {
-       return saveUser() + updateUser() + deleteUser();
+    public String getAnswer() {
+        return saveUser() + updateUser() + deleteUser();
     }
-
 
 }
